bcdde9e4ba801201fd0c263b86f070588de842e9
Haru <adityakhandagale69@gmail.com>
2026-08-05 14:01:42 +0530

    using device mac for identifying

--- services/socket_service.js
+++ services/socket_service.js
@@ -5,9 +5,11 @@ let io;
 const ioConnection = (server) => {
     io = new Server(server, {
         cors: {
-            origin: process.env.FRONTEND_URL,
+            origin: process.env.FRONTEND_URL || "*",
             methods: ["GET", "POST", "DELETE", "OPTIONS"],
-        }
+            credentials: true
+        },
+        transports: ['websocket', 'polling']
     });
 
     io.on("connection", (socket) => {
@@ -15,6 +17,10 @@ const ioConnection = (server) => {
 
         socket.on("disconnect", () => {
             console.log("Client disconnected:", socket.id);
+        });
+
+        socket.on("error", (error) => {
+            console.error("Socket error:", socket.id, error);
         });
     });
 
